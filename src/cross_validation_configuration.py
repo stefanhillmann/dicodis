e9f6d2fc98cdca3797f598561805f9c9af36fd67
Stefan Hillmann <stefan.hillmann@tu-berlin.de>
2013-08-20 12:00:51 +0200

    Configuration for matlabcluster run.

--- src/cross_validation_configuration.py
+++ src/cross_validation_configuration.py
@@ -1,35 +1,35 @@
 from ngram import NGramSize
 from classifier import ClassifierName
 
-validation_processes = 4
+validation_processes = 7
 
 sizes = [
-              #NGramSize.ONE,
-              #NGramSize.TWO,
-              #NGramSize.THREE,
-              #NGramSize.FOUR,
-              #NGramSize.FIVE,
-              #NGramSize.SIX,
-              #NGramSize.SEVEN,
+              NGramSize.ONE,
+              NGramSize.TWO,
+              NGramSize.THREE,
+              NGramSize.FOUR,
+              NGramSize.FIVE,
+              NGramSize.SIX,
+              NGramSize.SEVEN,
               NGramSize.EIGHT
              ]
 
 classifier_names = [
                     ClassifierName.COSINE,
-                    #ClassifierName.JENSEN,
-                    #ClassifierName.KULLBACK_LEIBLER,
-                    #ClassifierName.MEAN_KULLBACK_LEIBLER,
-                    #ClassifierName.SYMMETRIC_KULLBACK_LEIBLER
+                    ClassifierName.JENSEN,
+                    ClassifierName.KULLBACK_LEIBLER,
+                    ClassifierName.MEAN_KULLBACK_LEIBLER,
+                    ClassifierName.SYMMETRIC_KULLBACK_LEIBLER
                     ]
 
 frequency_thresholds = [
                         1,
-                        #2,
-                        #3,
-                        #4,
-                        #5,
-                        #6,
-                        #7
+                        2,
+                        3,
+                        4,
+                        5,
+                        6,
+                        7
                         ]
 
 class Configuration:
