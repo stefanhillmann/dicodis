2bdd64de5caa18ad29a86913e6fc214b73ae3014
Stefan Hillmann <stefan.hillmann@tu-berlin.de>
2015-08-07 08:32:42 +0200

    Revert "Weiter Auswertung"
    This reverts commit d60a19a9d1be3061e01e92229135dbef97877864.

--- src/boris_analysis/stem_leaf_performance.R
+++ src/boris_analysis/stem_leaf_performance.R
@@ -6,8 +6,9 @@ cross_validation <- mongoDbConnect("classification_cross_validation", "localhost
 classifier <- dbGetDistinct(cross_validation, "performance", key = 'classifier_name')
 
 for (c in classifier) {
-  print(sprintf("Classifier: %s", c))
+  print(c)
   query = sprintf('{"classifier_name": "%s"}', c)
-  p.c <- dbGetQuery(cross_validation, "performance", query, 0, 0)
-  stem(p.c$f_measure)
+  print(query)
+  #p.c <- dbGetQuery(cross_validation, "performance", '{}', 0, 0)
+  #stem()
 }
