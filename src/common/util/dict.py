0b645898391253f9409ef8bccdee68c6aa5e8e99
Stefan Hillmann <stefan.hillmann@tu-berlin.de>
2015-12-01 16:10:45 +0100

    mixed -ngram models

--- src/common/util/dict.py
+++ src/common/util/dict.py
@@ -1,6 +1,14 @@
 import collections
+import operator
+
 
 def sort_by_key(d):
     ordered_dict = collections.OrderedDict( sorted(d.items()) )
     
     return ordered_dict
+
+
+def sort_by_value(d):
+    sorted_d = sorted(d.items(), key=operator.itemgetter(1))
+
+    return sorted_d
